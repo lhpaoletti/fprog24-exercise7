d0974eca1c2ac0c94b895c53bded30969d44a862
Luis Henrique Paoletti <lh.paoletti@gmail.com>
2024-11-29 19:58:34 +0100

    feat: add Landeshauptstadt type and its related types

---
+++ src/landeshauptstadt.hs
@@ -0,0 +1,22 @@
+-- Landeshauptstadt Typ Datei
+-- Datum: 29.11.
+module Landeshauptstadt where
+
+
+data Landeshauptstadt = B | E | G | I | K | L | P | S | W
+    deriving (Eq, Ord, Show, Read, Enum, Bounded)
+
+-- Man darf nicht `deriving` fuer `type` Deklarationen
+--  nutzen, weil es nur ein Typsynonym ist
+type Staedtepaar = (Landeshauptstadt, Landeshauptstadt)
+
+data Staedtepaar1 = SP1 Landeshauptstadt Landeshauptstadt
+    deriving (Eq, Ord, Show, Read, Bounded)
+
+newtype Staedtepaar2 = SP2 (Landeshauptstadt, Landeshauptstadt)
+    deriving (Eq, Ord, Show, Read, Bounded)
+
+-- Man kann nicht `deriving` fuer Typen nutzen,
+--  die charakteristische Funktionen sind
+data Staedtepaar3 = SP3 ((Landeshauptstadt, Landeshauptstadt) -> Bool)
+data Staedtepaar4 = SP4 (Landeshauptstadt -> Landeshauptstadt -> Bool)
