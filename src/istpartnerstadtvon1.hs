dc420c1410c2fd31a104975df70c6019abe4df27
Luis Henrique Paoletti <lh.paoletti@gmail.com>
2024-12-03 09:00:17 +0100

    feat: implement Menge for IstPartnerstadtVon1

--- src/istpartnerstadtvon1.hs
+++ src/istpartnerstadtvon1.hs
@@ -21,3 +21,13 @@ instance Menge (MT1 Staedtepaar) where
     zieheab = zieheabMT1
     istTeilmenge = istTeilmengeMT1
     zeige = zeigeMT1
+
+instance Menge IstPartnerstadtVon1 where
+    leereMenge = IPV1 leereMenge
+    allMenge = IPV1 allMenge
+    istMenge (IPV1 m) = istMenge m
+    vereinige (IPV1 m1) (IPV1 m2) = IPV1 $ vereinige m1 m2
+    schneide (IPV1 m1) (IPV1 m2) = IPV1 $ schneide m1 m2
+    zieheab (IPV1 m1) (IPV1 m2) = IPV1 $ zieheab m1 m2
+    istTeilmenge (IPV1 m1) (IPV1 m2) = istTeilmenge m1 m2
+    zeige (IPV1 m) = zeige m
