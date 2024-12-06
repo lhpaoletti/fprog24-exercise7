bd2e0cc6779782927c68b70678aa398d2cd5653f
Luis Henrique Paoletti <lh.paoletti@gmail.com>
2024-12-06 09:57:59 +0100

    feat: implement Menge for IstPartnerstadtVon3

--- src/istpartnerstadtvon3.hs
+++ src/istpartnerstadtvon3.hs
@@ -8,3 +8,13 @@ import MT3
 
 
 data IstPartnerstadtVon3 = IPV3 (MT3 Staedtepaar)
+
+instance Menge IstPartnerstadtVon3 where
+    leereMenge = IPV3 leereMenge
+    allMenge = IPV3 allMenge
+    istMenge (IPV3 m) = istMenge m
+    vereinige (IPV3 m1) (IPV3 m2) = IPV3 $ vereinige m1 m2
+    schneide (IPV3 m1) (IPV3 m2) = IPV3 $ schneide m1 m2
+    zieheab (IPV3 m1) (IPV3 m2) = IPV3 $ zieheab m1 m2
+    istTeilmenge (IPV3 m1) (IPV3 m2) = istTeilmenge m1 m2
+    zeige (IPV3 m) = zeige m
