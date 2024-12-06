ff5daa8927235676c1a5d764c6f272f80e9e3d8c
Luis Henrique Paoletti <lh.paoletti@gmail.com>
2024-12-06 11:37:49 +0100

    style: reformat istpartnerstadtvon1 and -3

--- src/istpartnerstadtvon1.hs
+++ src/istpartnerstadtvon1.hs
@@ -11,13 +11,13 @@ data IstPartnerstadtVon1 = IPV1 (MT1 Staedtepaar)
 
 instance Menge IstPartnerstadtVon1 where
     leereMenge = IPV1 leereMenge
-    allMenge = IPV1 allMenge
+    allMenge   = IPV1 allMenge
     istMenge (IPV1 m) = istMenge m
-    vereinige (IPV1 m1) (IPV1 m2) = IPV1 $ vereinige m1 m2
-    schneide (IPV1 m1) (IPV1 m2) = IPV1 $ schneide m1 m2
-    zieheab (IPV1 m1) (IPV1 m2) = IPV1 $ zieheab m1 m2
-    istTeilmenge (IPV1 m1) (IPV1 m2) = istTeilmenge m1 m2
-    zeige (IPV1 m) = zeige m
+    zeige    (IPV1 m) =    zeige m
+    vereinige    (IPV1 m1) (IPV1 m2) = IPV1 $ vereinige m1 m2
+    schneide     (IPV1 m1) (IPV1 m2) = IPV1 $  schneide m1 m2
+    zieheab      (IPV1 m1) (IPV1 m2) = IPV1 $   zieheab m1 m2
+    istTeilmenge (IPV1 m1) (IPV1 m2) =     istTeilmenge m1 m2
 
 instance Relation IstPartnerstadtVon1 where
     istLinkstotal  (IPV1 r) = istLinkstotal  r
--- src/istpartnerstadtvon3.hs
+++ src/istpartnerstadtvon3.hs
@@ -11,13 +11,13 @@ data IstPartnerstadtVon3 = IPV3 (MT3 Staedtepaar)
 
 instance Menge IstPartnerstadtVon3 where
     leereMenge = IPV3 leereMenge
-    allMenge = IPV3 allMenge
+    allMenge   = IPV3 allMenge
     istMenge (IPV3 m) = istMenge m
-    vereinige (IPV3 m1) (IPV3 m2) = IPV3 $ vereinige m1 m2
-    schneide (IPV3 m1) (IPV3 m2) = IPV3 $ schneide m1 m2
-    zieheab (IPV3 m1) (IPV3 m2) = IPV3 $ zieheab m1 m2
-    istTeilmenge (IPV3 m1) (IPV3 m2) = istTeilmenge m1 m2
-    zeige (IPV3 m) = zeige m
+    zeige    (IPV3 m) =    zeige m
+    vereinige    (IPV3 m1) (IPV3 m2) = IPV3 $ vereinige m1 m2
+    schneide     (IPV3 m1) (IPV3 m2) = IPV3 $  schneide m1 m2
+    zieheab      (IPV3 m1) (IPV3 m2) = IPV3 $   zieheab m1 m2
+    istTeilmenge (IPV3 m1) (IPV3 m2) =     istTeilmenge m1 m2
 
 instance Relation IstPartnerstadtVon3 where
     istLinkstotal  (IPV3 r) = istLinkstotal  r
