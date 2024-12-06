e8161f18d223c5eadb34adeb2324790e12cc1eb4
Luis Henrique Paoletti <lh.paoletti@gmail.com>
2024-12-06 09:53:12 +0100

    refactor: move implementation of Relation for MT1 Staedtepaar to mt1.hs

--- src/istpartnerstadtvon1.hs
+++ src/istpartnerstadtvon1.hs
@@ -22,50 +22,6 @@ instance Menge IstPartnerstadtVon1 where
 
 
 
-instance Relation (MT1 Staedtepaar) where
-
-    istLinkstotal (MT1 pairs) =
-        -- Bilde eine Menge mit den linken Landeshauptstaedten, und vergleiche sie mit der Allmenge
-        sindGleich allMenge . MT1 . nub . map (fst) $ pairs
-
-    istRechtstotal (MT1 pairs) =
-        -- Bilde eine Menge mit den rechten Landeshauptstaedten, und vergleiche sie mit der Allmenge
-        sindGleich allMenge . MT1 . nub . map (snd) $ pairs
-
-    istReflexiv (MT1 pairs) =
-        -- Bilde eine Menge mit den reflexiven Landeshauptstaedten,
-        --  und vergleiche sie mit der Allmenge
-        sindGleich allMenge . MT1 $ reflexiveCities
-        where
-            reflexiveCities = [fst pair | pair <- pairs, fst pair == snd pair]
-
-
-    istSymmetrisch r = istSymmetrisch' r r
-        where
-            -- Pruefe Symmetrie von m gegen die urspruengliche Menge.
-            istSymmetrisch' :: MT1 Staedtepaar -> MT1 Staedtepaar -> Bool
-            istSymmetrisch'         _  (MT1 []) = True
-            istSymmetrisch' (MT1 orig) (MT1 ((l, l'):ps)) =
-                -- Schaue ob die Symmetrie fuer das erste Paar gilt, dann schau weiter
-                (l', l) `elem` orig && (istSymmetrisch' (MT1 orig) . MT1) ps
-
-
-    istTransitiv r = istTransitiv' r r
-        where
-            -- Pruefe Transitivitaet von m gegen die urspruengliche Menge.
-            istTransitiv' :: MT1 Staedtepaar -> MT1 Staedtepaar -> Bool
-            istTransitiv'         _  (MT1 []) = True
-            istTransitiv' (MT1 orig) (MT1 ((l, l'):ps)) =
-                -- Schaue ob die Transitivitaet aller (l R l'')s gilt, dann schau weiter
-                all (`elem` orig) l_l''s && (istTransitiv' (MT1 orig) . MT1) ps
-                where
-                    -- Alle vorgegebene (l' R x)s
-                    l'_l''s = filter (\pair -> fst pair == l') orig
-                    l''s    = map (snd) l'_l''s
-                    -- Alle zu pruefende Transitivitaeten aus l
-                    l_l''s  = [(l, l'') | l'' <- l''s]
-
-
 instance Relation IstPartnerstadtVon1 where
     istLinkstotal  (IPV1 r) = istLinkstotal  r
     istRechtstotal (IPV1 r) = istRechtstotal r
